f7f4a6a78b22fdfbc71f861fae0ceddbd2b43b9a
Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
2022-05-14 23:09:08 -0300

    Squashed commit of the following:
    commit 981f3d1b71a34e3784e5b10bf2146f19cf6e9914
    Author: Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
    Date:   Sat May 14 23:07:44 2022 -0300
    
        icon adicionado
    
    commit b94eaf6b95596bc0802f0239e8ab632273149b07
    Author: Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
    Date:   Sat May 14 22:55:47 2022 -0300
    
        endGame e restart feitos
    
    commit 4d83103a7efe870957faf63df4cb31870cc8037e
    Author: Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
    Date:   Sat May 14 19:01:27 2022 -0300
    
        checkGame feito
    
    commit c3e8fb2acc1ce9056cc1421c43ad797434708221
    Author: Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
    Date:   Tue May 10 18:15:32 2022 -0300
    
        js flipped consertado
    
    commit 7bde285abd4e8a9ba616c4d554ff230272de0e56
    Author: Isabela Ribeiro Baséggio <isabela.baseggio7@gmail.com>
    Date:   Tue May 10 15:05:02 2022 -0300
    
        js

--- script/script.js
+++ script/script.js
@@ -1,139 +1,199 @@
-// // Classes
-// const FRONT = "colorFront";
-// const BACK = "colorBack";
-// const CORRECT = "correct";
-// const WRONG = "wrong";
+// Classes
+const FRONT = "colorFront";
+const BACK = "colorBack";
+const CORRECT = "correct";
+const WRONG = "wrong";
+const COLOR = "color";
 
-let colorsRand = ["", "", "", ""];
+let colorsBoard = null;
+
+let colorElement = null;
+
+let colorElementFace = null;
+
+let hex = null;
+
+let questionHex = null;
+
+let gameOver = null;
+
+let round = 1;
+
+let score = 0;
 
 
-// function startGame() {
+// iniciando o teste
 
-//     createColors();
+function startGame() {
 
-//     let hideBtn = document.getElementById("start");
-//     hideBtn.style.display = 'none';
+    test.createColors();
 
-//     let showTest = document.getElementById("main");
-//     showTest.style.display = 'inline-block';
+    questionColor()
+
+    let hideBtn = document.getElementById("start");
+    hideBtn.style.display = 'none';
+
+    let showTest = document.getElementById("containerQuestion");
+    showTest.style.display = 'block';
+
+
+    cards = test.createCardColors();
+
+    initializeCards(test.cards);
 
 }
 
-// // Gerando as colores aleatoriamente e colocando-as no array
+// criando o visual das opções das cores
 
-// function createColors() {
+function initializeCards() {
 
-//     const letters = '0123456789ABCDEF';
-//     let hex = '#';
-//     let i = 0;
 
-//     while(i < 4) {
-//         for (let j = 0; j < 6; j++) {
-//             hex += letters[Math.floor(Math.random() * 16)];
-//         }
-//         colorsRand[i] = hex;
-//         hex = '#';
-//         i++;
-//     }
+    colorsBoard = document.getElementById("colorsBoard"); // não funciona o appendChild se o elemento pai for pego pela Class(explicação não sei, mas fica a dica)
 
-    initializeSpaceColors(colorsRand);
+
+    test.cards.forEach(card => {
+
+        colorElement = document.createElement("div");
+        colorElement.classList.add(COLOR);
+        colorElement.id = card.id;
+        colorElement.dataset.hex = card.id;
+
+
+
+        createSpaceContent(card, colorElement);
+
+        colorElement.addEventListener('click', colorResult);
+
+        colorsBoard.appendChild(colorElement);
+
+    });
+
 
 }
 
-// Gerando as colores aleatoriamente e colocando-as no array
+//  criando o Front e Back das cores
 
-function createColors() {
+function createSpaceContent(card, colorElement) {
 
-    const letters = '0123456789ABCDEF';
-    let hex = '#';
-    let i = 0;
+    createSpaceFace(FRONT, card, colorElement);
+    createSpaceFace(BACK, card, colorElement);
 
-    while(i < 4) {
-        for (let j = 0; j < 6; j++) {
-            hex += letters[Math.floor(Math.random() * 16)];
+}
+
+function createSpaceFace(face, card, element) {
+
+    colorElementFace = document.createElement('div');
+    colorElementFace.classList.add(face);
+
+    if (face == FRONT) {
+        colorElementFace.style.backgroundColor = "#" + card.id;
+        colorElementFace.style.cursor = 'pointer';
+    } else {
+        colorElementFace.innerHTML = "<strong>" + card.id + "</strong>";
+        colorElementFace.style.color = "#" + card.id;
+        if(card.id == hex) {
+            colorElementFace.classList.add(CORRECT);
+        } else {
+            colorElementFace.classList.add(WRONG);
         }
-        colorsRand[i] = hex;
-        hex = '#';
-        i++;
     }
 
-    initializeSpaceColors(colorsRand);
+    element.appendChild(colorElementFace);
 
-    questionColor(colorsRand);
-
-    return colorsRand;
-
-// }
-
-function initializeSpaceColors(spaces) {
-
-    let containerColors = document.getElementById("containerColors");
-
-    spaces.forEach( space => {
-
-        let spaceElement = document.createElement('div');
-        spaceElement.classList.add(FRONT);
-
-        createSpaceContent(space, spaceElement);
-
-        spaceElement.addEventListener('click', colorResult)
-
-        containerColors.appendChild(spaceElement);
-        
-        
-    });
-
-}
-
-function initializeSpaceColors(spaces) {
-
-    let containerColors = document.getElementById("containerColors");
-
-    spaces.forEach( space => {
-
-        let spaceElement = document.createElement('div');
-        spaceElement.classList.add(FRONT);
-
-        createSpaceContent(space, spaceElement);
-
-        spaceElement.addEventListener('click', colorResult)
-
-        containerColors.appendChild(spaceElement);
-        
-        
-    });
-
-}
-
-function createSpaceContent(colorsRand, spaceElement){
-
-    createSpaceFront(FRONT, colorsRand, spaceElement);
-    createSpaceBack(BACK, colorsRand, spaceElement);
-
-}
-
-function createSpaceFront(face, colorsRand, element){
-
-    let spaceElementFace = document.createElement('div');
-    spaceElementFace.classList.add(FACE);
 
 }
 
 
 // Sorteando o Hexadecimal da pergunta
 
-function questionColor(colors) {
+function questionColor() {
 
-    let sortHex = colors[Math.floor(Math.random() * (2 + 1))];
+    hex = test.sortHex();
 
-    let questionHex = document.getElementsByClassName("hex");
-    questionHex.innerHTML = sortHex;
+    questionHex = document.getElementById("hex");
+    questionHex.innerHTML = "<strong>" + hex + "</strong>";
+    // questionHex.style.boxShadow = "none";
 
-    console.log(typeof sortHex)
-    console.log(typeof questionHex)
+    return hex;
 
-// }
+
+}
+
+// Mostrando as repostas e chegando a opção selecionada
 
 function colorResult() {
+
+    test.cards.forEach( card => {
+        card.flipped = true;
+    })
+
+
+    if (test.setCard(this.id)) {
+        let frontCards = document.querySelectorAll('.colorFront');
+
+        frontCards.forEach(frontCard => {
+            frontCard.style.display = "none";
+        })
+
+        let backCards = document.querySelectorAll('.colorBack');
+
+        backCards.forEach(backCard => {
+            backCard.style.display = "flex";
+        })
+
+
+        if(test.checkMatch()) {
+            questionHex.classList.add(CORRECT);
+            score++;
+        } else {
+            questionHex.classList.add(WRONG);
+        }
+    }
+    
+    setTimeout(()=>{
+
+        if(round < 10){
+            newRound();
+            console.log(round);
+        } else {
+            endGame();
+        }
+
+    }, 2500)
+
+}
+
+function newRound() {
+
+    round++;
+    questionHex.className = '';
+    colorsBoard.innerHTML = "";
+    test.clearCard();
+    startGame();
+
+}
+
+
+function endGame() {
+
+        gameOver = document.getElementById("result");
+        gameOver.style.display = 'inline-block';
+
+        let result = document.getElementById("h2");
+        result.innerHTML = "Your score was " + score + "/10";
+
+}
+
+function restart() {
+
+    questionHex.className = '';
+    colorsBoard.innerHTML = "";
+    test.clearCard();
+
+    gameOver.style.display = 'none';
+
+    round = 1;
+
+    startGame();
 
 }
---
+++ script/test.js
@@ -0,0 +1,102 @@
+let test = {
+
+    sortHexa: null,
+
+    lockMode: false,
+
+    setedCard: null,
+
+    colorsRand: [],
+
+    cards: null,
+
+    // setando a opção selecionada e restringindo a seleção para apenas uma resposta
+
+    setCard: function(id) {
+
+        let card = this.cards.filter(card=>card.id==id)[0];
+
+
+        if(card.fippled || this.lockMode) {
+            return false;
+        }
+
+        if(!this.setedCard) {
+            this.setedCard = card;
+            this.lockMode = true;
+            return true;
+        }
+
+
+    },
+
+    // checando se a opção selecionada é a resposta certa
+
+    checkMatch: function() {
+        return this.setedCard.id == this.sortHexa;
+    },
+
+    // resetando a opção selecionada
+
+    clearCard: function() {
+        this.setedCard = null;
+        this.lockMode = null;
+    },
+
+    // Gerando as colores aleatoriamente e colocando-as no array
+
+    createColors: function () {
+        this.colorsRand = [];
+        const letters = '0123456789ABCDEF';
+        let hex = "";
+        let i = 0;
+
+        while (i < 4) {
+            for (let j = 0; j < 6; j++) {
+                hex += letters[Math.floor(Math.random() * 16)];
+            }
+            this.colorsRand[i] = hex;
+            hex = "";
+            i++;
+        }
+
+    },
+
+    // Sorteando a cor da pergunta
+
+    sortHex: function(){
+
+        this.sortHexa = this.colorsRand[Math.floor(Math.random() * (2 + 1))];
+
+        return this.sortHexa;
+
+    },
+
+    // criando as cores como opções
+
+    createCardColors: function () {
+
+        this.cards = [];
+
+        this.colorsRand.forEach( (color) => {
+            this.cards.push(this.insertData(color))
+        })
+
+        return this.cards;
+
+    },
+
+    // inserindo o Id e background referente a cada opção de cor
+
+    insertData: function (color) {
+
+        return {
+            id: color,
+            bg: color,
+            flipped: false
+        }
+
+    }
+
+
+}
